f75bb2dfd47ab9fdccaee784fc4b1f6250f118db
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-01 18:41:03 +0900

    Rubocopを実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers' }
 
   with_options presence: true do
@@ -20,5 +20,4 @@ class User < ApplicationRecord
       validates :family_name_kana
     end
   end
-
 end
