b7a0365590b3736d141cc3fd151f46664b099dce
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-01 18:37:05 +0900

    バリデーションをwith_options形式で統合

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, :first_name, :family_name, :first_name_kana, :family_name_kana, :birth_day, presence: true
-  validates :first_name, :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
-  validates :first_name_kana, :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
+  
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers' }
+
+  with_options presence: true do
+    validates :nickname, uniqueness: { case_sensitive: true }
+    validates :birth_day
+
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
+      validates :first_name
+      validates :family_name
+    end
+
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' } do
+      validates :first_name_kana
+      validates :family_name_kana
+    end
+  end
+
 end
