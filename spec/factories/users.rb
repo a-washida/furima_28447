985b7072b97dd2ecdc44cea9336a8ab0d2e9df7e
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-01 15:56:56 +0900

    FactoryBotのpasswordをFakerを使わない形式に変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :user do
     nickname {Faker::Name.name}
     email {Faker::Internet.free_email}
-    password = Faker::Internet.password(min_length: 8)
+    password = "a1234z"
     password {password}
     password_confirmation {password}
     family_name {Faker::Name.last_name }
