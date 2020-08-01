6e1fb8aa9ee7b7a36cc5b3fa91fc238afab12736
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-01 14:36:08 +0900

    passwordに半角英数字混合のバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   validates :nickname, :first_name, :family_name, :first_name_kana, :family_name_kana, :birth_day, presence: true
   validates :first_name, :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
   validates :first_name_kana, :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: '6文字以上の半角英数字混合で設定してください' }
 end
