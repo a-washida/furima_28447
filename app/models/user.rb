79a732518bfc1b1c626328e8114462e034d12338
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-01 12:28:35 +0900

    バリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, :first_name, :family_name, :first_name_kana, :family_name_kana, :birth_day, presence: true
+  validates :first_name, :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
+  validates :first_name_kana, :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' }
 end
