0921c43caf0b0832ada071a8490d3fe85191cb10
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-02 16:46:12 +0900

    FactoryBotでitemの値生成用のファイルを作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name { Faker::Name.name }
+    description { Faker::Name.name }
+    category_id { Faker::Number.within(range: 2..11) }
+    status_id { Faker::Number.within(range: 2..7) }
+    shipping_fee_id { Faker::Number.within(range: 2..3) }
+    prefecture_id { Faker::Number.within(range: 2..48) }
+    day_until_shipping_id { Faker::Number.within(range: 2..4) }
+    price { Faker::Number.within(range: 300..9999999) }
+    association :user
   end
 end
