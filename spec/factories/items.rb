5422ceb4d4923a3b593404c80055516e4ef5fb92
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-02 18:53:55 +0900

    FakerのダミーデータをFakerではなく直接データを入力するように変更

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :item do
-    name { Faker::Name.name }
-    description { Faker::Name.name }
-    category_id { Faker::Number.within(range: 2..11) }
-    status_id { Faker::Number.within(range: 2..7) }
-    shipping_fee_id { Faker::Number.within(range: 2..3) }
-    prefecture_id { Faker::Number.within(range: 2..48) }
-    day_until_shipping_id { Faker::Number.within(range: 2..4) }
-    price { Faker::Number.within(range: 300..9_999_999) }
+    name { 'hoge' }
+    description { 'hogefuga' }
+    category_id { 3 }
+    status_id { 2 }
+    shipping_fee_id { 2 }
+    prefecture_id { 10 }
+    day_until_shipping_id { 3 }
+    price { 500 }
     association :user
   end
 end
