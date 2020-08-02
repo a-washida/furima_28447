4ec556ca172378d69417ae278fe639d1fdbdd946
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-02 12:50:55 +0900

    ログインしているユーザーだけが出品ページに遷移できるように設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :move_to_new_user_session, except: [:index, :show]
 
   def index
   end
@@ -21,5 +22,11 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :day_until_shipping_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_new_user_session
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
