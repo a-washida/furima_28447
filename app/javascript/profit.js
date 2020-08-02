864bd15454b50a71493279edfcc79104d290f416
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-02 17:48:09 +0900

    価格に半角数字以外を入力するとアラートが出るように変更

--- app/javascript/profit.js
+++ app/javascript/profit.js
@@ -7,8 +7,13 @@ function calculate_profit() {
   price.addEventListener("input", (e) => {
     let fee = document.getElementById("add-tax-price");
     let profit = document.getElementById("profit");
-    fee.innerHTML = price.value * 0.1;
-    profit.innerHTML = price.value * 0.9
+    if (price.value.match(/^[0-9]+$/)) {
+      fee.innerHTML = price.value * 0.1;
+      profit.innerHTML = price.value * 0.9
+    } else {
+      fee.innerHTML = "半角数字のみ入力可能"
+      profit.innerHTML = "半角数字のみ入力可能"
+    }
 
     e.preventDefault();
     });
