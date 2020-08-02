0adc893fdc1ed76274103a7cfb9e9e228510e7a6
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-02 17:28:58 +0900

    itemモデルの単体テストコードを作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,7 +20,7 @@ class Item < ApplicationRecord
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   end
 
-  with_options numericality: { other_than: 1 } do
+  with_options numericality: { other_than: 1, message: "Select" } do
     validates :category_id
     validates :status_id
     validates :shipping_fee_id
