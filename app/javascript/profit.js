b22728525e3310fb58f0904f951924e43eee8400
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-02 17:30:24 +0900

    windowをloadではイベント発火しない場合があったのでsetIntervalに変更

--- app/javascript/profit.js
+++ app/javascript/profit.js
@@ -1,11 +1,17 @@
 function calculate_profit() {
   const price = document.getElementById("item_price");
+  if (price.getAttribute("data-load") != null) {
+    return null;
+  }
+  price.setAttribute("data-load", "true");
   price.addEventListener("input", (e) => {
     let fee = document.getElementById("add-tax-price");
     let profit = document.getElementById("profit");
     fee.innerHTML = price.value * 0.1;
     profit.innerHTML = price.value * 0.9
+
+    e.preventDefault();
     });
 }
 
-window.addEventListener('load', calculate_profit);
+setInterval(calculate_profit, 1000);
