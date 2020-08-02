add962ba2c71eb913b9698abfbfdc564fd79d9ab
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-02 10:45:03 +0900

    ActiveHash関連のアソシエーションとバリデーションを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,24 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :genre
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :shipping_fee
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :day_until_shipping
+
+  with_options presence: true do
+    with_options numericality: { other_than: 1 } do
+      validates :category
+      validates :status
+      validates :shipping_fee
+      validates :prefecture
+      validates :day_until_shipping
+    end
+  end
 
   #空の投稿を保存できないようにする
-  validates :category, presence: true
+  # validates :category, presence: true
 
   #ジャンルの選択が「--」の時は保存できないようにする
-  validates :category_id, numericality: { other_than: 1 }
+  # validates :category_id, numericality: { other_than: 1 }
 end
