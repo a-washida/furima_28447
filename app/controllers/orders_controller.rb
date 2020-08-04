33114cb633774629f0f7a69763a0768d863dc433
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-04 19:27:25 +0900

    renderで購入画面が表示された時に住所情報をリセットする機能を実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,6 +14,7 @@ class OrdersController < ApplicationController
       @order.save
       return redirect_to root_path
     else
+      @order = OrderDestination.new
       render :index
     end
   end
