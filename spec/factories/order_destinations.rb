57c941669eac75e5d36ab7f218e7ed1d5795c7b3
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-04 23:07:34 +0900

    単体テストコードを実行

---
+++ spec/factories/order_destinations.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_destination do
+    postal_code { "111-1111" }
+    prefecture_id { 2 }
+    city { "大阪" }
+    address { "北区" }
+    building_name { "柳ビル" }
+    phone_number { "08011112222" }
+  end
+end
