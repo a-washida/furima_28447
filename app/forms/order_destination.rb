ca8cc88c4421e70d96eb86e8accc5f098942e169
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-04 15:10:45 +0900

    フォームオブジェクトorder_destination.rbを修正

--- app/forms/order_destination.rb
+++ app/forms/order_destination.rb
@@ -1,12 +1,10 @@
 class OrderDestination
 
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number
+  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number
 
   def save
-    item = Item.find(params[:id])
-    user = item.user
-    Order.create(user_id: user.id, item_id: item.id)
-    Destination.create(item_id: item.id, postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number)
+    Order.create(user_id: user_id, item_id: item_id)
+    Destination.create(item_id: item_id, postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number)
   end
 end
