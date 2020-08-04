ab371e9282385673d0c2feedecc9ae172d4b2b84
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-04 22:39:37 +0900

    バリデーションを修正

--- app/forms/order_destination.rb
+++ app/forms/order_destination.rb
@@ -5,13 +5,11 @@ class OrderDestination
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ , message: "Input correctly"}
-    # validates :prefecture
+    validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
     validates :city
     validates :address
     validates :phone_number, length: { maximum: 11 }
   end
-
-  validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
 
   def save
     Order.create(user_id: user_id, item_id: item_id)
