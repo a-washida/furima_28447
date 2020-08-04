f60107d5633531478ba8d2cc4678cd21d0e28935
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-04 19:13:34 +0900

    バリデーションを設定

--- app/forms/order_destination.rb
+++ app/forms/order_destination.rb
@@ -3,6 +3,16 @@ class OrderDestination
   include ActiveModel::Model
   attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number
 
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ , message: "Input correctly"}
+    # validates :prefecture
+    validates :city
+    validates :address
+    validates :phone_number, length: { maximum: 11 }
+  end
+
+  validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
+
   def save
     Order.create(user_id: user_id, item_id: item_id)
     Destination.create(item_id: item_id, postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number)
