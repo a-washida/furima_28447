7583e2d3ed941e69fcc0f5d14cff00f2b650d9b4
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-04 15:11:48 +0900

    商品購入機能を実装(クレジットカード無し)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,22 @@
 class OrdersController < ApplicationController
+  def index
+    @item = Item.find(params[:item_id])
+    @order = OrderDestination.new
+  end
+
+  def create
+    @order = OrderDestination.new(order_params)
+    if @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_destination).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
+  end
+
 end
