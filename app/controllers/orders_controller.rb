9b48f3f576265e4ccffb2a891ce8f7822ad75078
a-washida <59239304+a-washida@users.noreply.github.com>
2020-08-04 18:07:43 +0900

    購入時にクレジットカード機能を追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,18 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
     @order = OrderDestination.new
   end
 
   def create
-    @order = OrderDestination.new(order_params)
-    if @order.save
-      redirect_to root_path
+    order_params_delete_token = order_params
+    order_params_delete_token.delete(:token)
+    @order = OrderDestination.new(order_params_delete_token)
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
     else
       render :index
     end
@@ -15,8 +20,21 @@ class OrdersController < ApplicationController
 
   private
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
   def order_params
-    params.require(:order_destination).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
+    params.require(:order_destination).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency:'jpy'
+    )
   end
 
 end
